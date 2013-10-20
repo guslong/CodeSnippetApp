674b7a482d22f11ab903267d1c55471374f4fb72
Angus Long <mail@anguslong.net>
2013-10-20 13:54:31 +0200

    recommit of the snippet manager class

--- CodeSnippetApp/src/test/SnippetManager.java
+++ CodeSnippetApp/src/test/SnippetManager.java
@@ -10,13 +10,21 @@ import model.Snippet;
 
 public class SnippetManager {
 
+	/** the static member for the snippetManager, singleton */
 	private static SnippetManager snippetManager = null;
+	
+	/** stores the snippets in an ArrayList */
 	private ArrayList<Snippet> snippets;
 
+	/** private constructor */
 	private SnippetManager() {
 		snippets = new ArrayList<>();
 	}
 
+	/** gets an instance of the snippet manager
+	 * if one already exists, return the existing object
+	 * (singleton pattern) */
+	
 	public static SnippetManager getInstance() {
 		if (snippetManager == null) {
 			snippetManager = new SnippetManager();
@@ -24,11 +32,13 @@ public class SnippetManager {
 		return snippetManager;
 	}
 
+	/** @return the ArrayList of snippets */
 	public ArrayList<Snippet> getSnippets() {
 		return snippets;
 		
 	}
 	
+	/** adds a snippet to the arraylist */
 	public void addSnippet(Snippet snippet) {
 		snippets.add(snippet);
 	}
