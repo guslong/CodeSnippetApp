3c1aaa155aad2e7c38f197e5a48d8e6bee4a3ca8
Angus Long <mail@anguslong.net>
2013-10-20 18:52:08 +0200

    bit of clean up

--- CodeSnippetApp/src/main/Application.java
+++ CodeSnippetApp/src/main/Application.java
@@ -10,14 +10,14 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-				
-		SnippetManager manager  = SnippetManager.getInstance();
-		Snippet newSnippet = new Snippet("Blah blah", "Java");
-		newSnippet.setSnippetText("class SecurityUtils {  private static final byte[] salt = { }");
-		
-		// test adding a snippet to the array and the database
-		manager.addSnippet(newSnippet);
-
+		test();	
 	}
 
+	public static void test() {
+	    SnippetManager manager  = SnippetManager.getInstance();
+		Snippet newSnippet = new Snippet("Blah blah", "Java");
+		newSnippet.setSnippetText("class SecurityUtils {  private static final byte[] salt = { }");
+		// test adding a snippet to the array and the database
+		manager.addSnippet(newSnippet);
+	}
 }
--- CodeSnippetApp/src/model/SnippetManager.java
+++ CodeSnippetApp/src/model/SnippetManager.java
@@ -10,62 +10,67 @@ import db.DatabaseManager;
 
 public class SnippetManager {
 
-	/**
-	 * the database manager to enable this object to communicate with the
-	 * database
-	 */
-	DatabaseManager dbm;
+    /**
+     * the database manager to enable this object to communicate with the
+     * database
+     */
+    DatabaseManager dbm;
 
-	/** the static member for the snippetManager, singleton */
-	private static SnippetManager snippetManager = null;
+    /** the static member for the snippetManager, singleton */
+    private static SnippetManager snippetManager = null;
 
-	/** stores the snippets in an ArrayList */
-	private ArrayList<Snippet> snippets;
+    /** stores the snippets in an ArrayList */
+    private ArrayList<Snippet> snippets;
 
-	/** private constructor */
-	private SnippetManager() {
-		snippets = new ArrayList<>(); // TODO in future this will initialise the
-										// arraylist from the database
-		dbm = new DatabaseManager(); // get an instance of the db manager
+    /** private constructor */
+    private SnippetManager() {
+	// TODO in future this will initialise the arraylist from the database
+	snippets = new ArrayList<>(); 
+	
+	dbm = new DatabaseManager(); // get an instance of the db manager
+    }
+
+    /**
+     * gets an instance of the snippet manager if one already exists, return the
+     * existing object (singleton pattern)
+     */
+
+    public static SnippetManager getInstance() {
+	if (snippetManager == null) {
+	    snippetManager = new SnippetManager();
 	}
+	return snippetManager;
+    }
 
-	/**
-	 * gets an instance of the snippet manager if one already exists, return the
-	 * existing object (singleton pattern)
-	 */
+    /** @return the ArrayList of snippets */
+    public ArrayList<Snippet> getSnippets() {
+	return snippets;
+    }
 
-	public static SnippetManager getInstance() {
-		if (snippetManager == null) {
-			snippetManager = new SnippetManager();
-		}
-		return snippetManager;
+    /**
+     * adds a snippet to the arraylist and to the database
+     * 
+     * @param the
+     *            snippet to add
+     */
+    public void addSnippet(Snippet snippet) {
+	snippets.add(snippet);
+
+	// build an insert query
+	String query = "insert into " + DatabaseManager.TABLE_NAME
+		+ "(SNIPPET_TITLE, SNIPPET_TEXT, SNIPPET_LANG) values " + "('"
+		+ snippet.getSnippetTitle() + "' ," + " '"
+		+ snippet.getSnippetText() + "' ," + " '"
+		+ snippet.getLanguage() + "');";
+
+	dbm.doInsertQuery(query);
+    }
+
+    // tester method to print out the snippets to sysout
+    public void printAll() {
+	for (Snippet snippet : snippets) {
+	    System.out.println(snippet.toString());
 	}
-
-	/** @return the ArrayList of snippets */
-	public ArrayList<Snippet> getSnippets() {
-		return snippets;
-
-	}
-
-	/** adds a snippet to the arraylist */
-	// TODO and add to the database
-	public void addSnippet(Snippet snippet) {
-		snippets.add(snippet);
-
-		// build a query to enable to insert into database
-		String query = "insert into " + DatabaseManager.TABLE_NAME
-				+ "(SNIPPET_TITLE, SNIPPET_TEXT, SNIPPET_LANG) values " + "('"
-				+ snippet.getSnippetTitle() + "' ," + " '"
-				+ snippet.getSnippetText() + "' ," + " '"
-				+ snippet.getLanguage() + "');";
-		dbm.doInsertQuery(query);
-	}
-
-	// tester method to print out the snippets to sysout
-	public void printAll() {
-		for (Snippet snippet : snippets) {
-			System.out.println(snippet.toString());
-		}
-	}
+    }
 
 }
